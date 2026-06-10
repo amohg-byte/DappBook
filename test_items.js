b372b441facc5d2f3d39b909b581856cf475d908
amohg-byte <amogbussy6969@gmail.com>
2026-06-10 11:27:56 +0530

    Update test_items.js

--- test_items.js
+++ test_items.js
@@ -4,7 +4,7 @@ async function main() {
   const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:8545");
   const contractAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
   const contractABI = [
-    "function items(uint256) external view returns (uint256 itemId, address owner, address renter, uint256 pricePerDay, uint256 depositAmount, string ipfsCID, uint8 status, uint256 rentedAt, uint256 returnedAt, uint256 disputeRaisedAt)"
+  "function items(uint256) external view returns (uint256 itemId, address owner, address renter, uint256 pricePerDay, uint256 depositAmount, string ipfsCID, uint8 status, uint256 rentedAt, uint256 returnedAt, uint256 disputeRaisedAt)"
   ];
   
   const contract = new ethers.Contract(contractAddress, contractABI, provider);
